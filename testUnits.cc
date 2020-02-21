2607673f2a96211be946aacac23bd44888785e60
Aoshiga <titouan.buronfosse@gmail.com>
2020-02-21 10:08:40 +0100

    Add some test and clean it

--- testUnits.cc
+++ testUnits.cc
@@ -13,12 +13,8 @@ int main(int argc, char *argv[]) {
 TEST(Cast, DeciToKilo) {
   phy::Qty <phy::Metre, std::deci> mm (101245);
   auto nm = phy::qtyCast <phy::Qty <phy::Metre , std::kilo >>(mm);
-
-  std::cout << nm.value << "\n";
-
   ASSERT_EQ(nm.value, 10);
 }
-
 
 TEST(Cast, FootToMeter){
   phy::Qty <phy::Metre, phy::Foot::Ratio> foot(42);
@@ -32,33 +28,18 @@ TEST(Cast, MeterToMilli){
   EXPECT_EQ(mm.value, 1000);
 }
 
-TEST(Metres, AddTwoMetres) {
+TEST(Units, Add) {
   using namespace phy::literals;
 
-  // auto velocity = 100000_metres / 3600_seconds; // 100 km/h
   phy::Length length = 1000_metres;
   phy::Length length2 = 100_metres;
 
   auto result = length2 + length;
 
-  std::cout << result.value << std::endl;
-
   EXPECT_EQ(result.value, 1100);
 }
 
-TEST(Metres, AddTwoMetres2) {
-  using namespace phy::literals;
-
-  auto val1 = phy::Qty<phy::Metre>(10);
-  auto val2 = phy::Qty<phy::Metre>(24);
-  auto val3 = val1 + val2;
-
-  auto resVal = phy::Qty<phy::Metre>(34);
-
-  ASSERT_EQ(val3.value, resVal.value);
-}
-
-TEST(Metres, AddEqual) {
+TEST(Units, AddEqual) {
   using namespace phy::literals;
 
   phy::Qty<phy::Metre> val1(10);
@@ -70,20 +51,17 @@ TEST(Metres, AddEqual) {
   ASSERT_EQ(val1.value, resVal.value);
 }
 
-TEST(Metres, SubstractTwoMetres) {
+TEST(Units, Substract) {
   using namespace phy::literals;
 
-  // auto velocity = 100000_metres / 3600_seconds; // 100 km/h
   phy::Length length = 1000_metres;
   phy::Length length2 = 100_metres;
   auto result = length2 - length;
 
-  std::cout << result.value << std::endl;
-
   EXPECT_EQ(result.value, -900);
 }
 
-TEST(Metres, SubstractEqual) {
+TEST(Units, SubstractEqual) {
   using namespace phy::literals;
 
   phy::Qty<phy::Metre> val1(30);
@@ -96,7 +74,7 @@ TEST(Metres, SubstractEqual) {
 }
 
 
-TEST(Metres, SubstractMetreMilli) {
+TEST(Units, SubstractMilli) {
   using namespace phy::literals;
 
   phy::Qty<phy::Metre> val1(30);
@@ -106,12 +84,12 @@ TEST(Metres, SubstractMetreMilli) {
   phy::Qty<phy::Metre,std::milli> resVal(29976);
 
   ASSERT_EQ(res.value, resVal.value);
-   auto res2 = val2 - val1;
+  auto res2 = val2 - val1;
 
   ASSERT_EQ(res2.value, -resVal.value);
 }
 
-TEST(Metres, SubstractFootMilli) {
+TEST(Units, SubstractFootMilli) {
   using namespace phy::literals;
 
   phy::Qty<phy::Metre,Foot::Ratio> val1(30);
@@ -123,63 +101,55 @@ TEST(Metres, SubstractFootMilli) {
   ASSERT_EQ(res.value, resVal.value);
 
 }
-TEST(Metres, MultiplyLengths) {
+
+TEST(Units, Multiply) {
   using namespace phy::literals;
 
-  // auto velocity = 100000_metres / 3600_seconds; // 100 km/h
   phy::Length length = 24_metres;
   phy::Length length2 = 84_metres;
   auto expected = 24 * 84;
   auto result = length2 * length;
 
-  std::cout << result.value << std::endl;
-
   EXPECT_EQ(result.value, expected);
 }
 
-TEST(Metres, MilliAdd) {
+TEST(Units, MilliAdd) {
   using namespace phy::literals;
 
-  // auto velocity = 100000_metres / 3600_seconds; // 100 km/h
   phy::Length length = 5_metres;
   phy::Qty<phy::Metre, std::milli> length2(3); // 3 mm
   auto result = length + length2;
-  std::cout << result.value << std::endl;
+
   EXPECT_EQ(result.value, 5003);
 }
 
-TEST(Metres, MilliMult) {
+TEST(Units, MilliMult) {
   using namespace phy::literals;
 
   phy::Length length = 5_metres;
   phy::Qty<phy::Metre, std::milli> length2(3); // 3 mm
   auto result = length * length2; //15000mm2
 
-  std::cout << result.value << std::endl;
   EXPECT_EQ(result.value, 15000);
 }
 
-TEST(Metres, Millidivide) {
+TEST(Units, Millidivide) {
   using namespace phy::literals;
 
-  // auto velocity = 100000_metres / 3600_seconds; // 100 km/h
-  phy::Length length = 5_metres;
+  phy::Length length = 6_metres;
   phy::Qty<phy::Metre, std::milli> length2(3); // 3 mm
   auto result = length / length2;
 
-  std::cout << result.value << std::endl;
-  // EXPECT_EQ(result.value, 15000);
+  EXPECT_EQ(result.value, 2000);
 }
 
-TEST(Ampere, CentiPlusDeci) {
+TEST(Units, CentiPlusDeci) {
   using namespace phy::literals;
 
-  // auto velocity = 100000_metres / 3600_seconds; // 100 km/h
   phy::Qty<phy::Ampere, std::milli> mm(50); // 50 mm
   phy::Qty<phy::Ampere, std::centi> cm(2);  // 2 cm
   auto result = mm + cm;
 
-  std::cout << result.value << std::endl;
   EXPECT_EQ(result.value, 70);
 }
 
@@ -203,26 +173,23 @@ TEST(Units, OperatorPlus) {
 TEST(Units, reusingTheSameQuantity) {
   phy ::Qty<phy::Metre, phy::Foot::Ratio> foot(42);
   phy ::Qty<phy::Metre> m(10);
-  phy ::Qty<phy::Metre, std::milli> mm(52);
 
-  auto footMetre = foot + m;
-  auto footMetreMetre = footMetre + m;
-  auto res3 = footMetre + foot;
+  auto footMetre = foot + m; //= 74foot
+  auto footMetreMetre = footMetre + m; //= 106foot
+  auto res3 = footMetre + foot; //= 116foot
 
   EXPECT_EQ(footMetre.value, 74);
   EXPECT_EQ(footMetreMetre.value, 106);
   EXPECT_EQ(res3.value, 116);
 }
 
-TEST(Length, addDiffrentRatios) {
+TEST(Units, addDiffrentRatios) {
   using namespace phy::literals;
 
   phy ::Qty<phy::Metre, phy::Foot::Ratio> foot(42);
   phy ::Qty<phy::Metre, std::milli> mm(52);
   auto res = foot + mm;
   EXPECT_EQ(res.value, 12853);
-
-
 }
 
 TEST(Units, MultipleAdd){
@@ -263,7 +230,7 @@ TEST(Units, MultipleMultiplicateFoot){
   phy :: Qty <phy::Metre ,std::milli> mm (1);
   auto res1 = cm * foot; //1 foot * 60cm = 1800cm2
   EXPECT_EQ(res1.value, 1800);
-  auto res3 = res1 * mm; // 1800cm2 * 1mm = 4000m
+  auto res3 = res1 * mm; // 1800cm2 * 1mm = 4000m3
   EXPECT_EQ(res3.value, 621);
 
 }
@@ -274,38 +241,41 @@ TEST(Units, MultipleDivide){
   phy :: Qty <phy::Metre > m (200);
   auto res1 = km / hm; //12km * 20hm = 6hm
   EXPECT_EQ(res1.value, 6);
-  auto res2 = res1 - m; // 6hm * 200m = 3m
+  auto res2 = res1 / m; // 6hm * 200m = 3m
   EXPECT_EQ(res2.value, 3);
 }
 
-TEST(Metres, MilliMultiAdd) {
+TEST(Units, MultipleDivideFoot){
+  phy :: Qty <phy::Metre , phy::Foot::Ratio > foot (1);
+  phy :: Qty <phy::Metre , std::centi > cm (3);
+  phy :: Qty <phy::Metre ,std::milli> mm (5);
+  auto res1 = cm / foot; //1 foot / 3cm = 10
+  EXPECT_EQ(res1.value, 10);
+  auto res3 = res1 / mm; // 10 / 5mm = 2mm-1
+  EXPECT_EQ(res3.value, 2);
+
+}
+
+TEST(Units, MilliMultiAdd) {
   using namespace phy::literals;
 
-  // auto velocity = 100000_metres / 3600_seconds; // 100 km/h
   phy::Length length = 5_metres;
   phy::Qty<phy::Metre, std::milli> length2(3); // 3 mm
   auto result = length + length2;
   auto result2 = result + length2;
-  std::cout << result.value << std::endl;
-  std::cout << result2.value << std::endl;
 
   EXPECT_EQ(result.value, 5003);
- EXPECT_EQ(result2.value, 5006);
+  EXPECT_EQ(result2.value, 5006);
 }
 
-
-
-TEST(Metres, MilliMultiAdd2) {
+TEST(Units, MilliMultiAdd2) {
   using namespace phy::literals;
 
-  // auto velocity = 100000_metres / 3600_seconds; // 100 km/h
   phy::Length length = 5_metres;
   phy::Qty<phy::Metre, std::milli> length2(3); // 3 mm
   auto result =  length2 + length;
   auto result2 =  length2 + result;
 
-  std::cout << result.value << std::endl;
-  std::cout << result2.value << std::endl;
-
   EXPECT_EQ(result.value, 5003);
+  EXPECT_EQ(result2.value, 5006);
 }
